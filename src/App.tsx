b6892af059ad7f67c351f43ed58ccb09518ab727
Phdoflip <antrodossantos@gmail.com>
2025-07-30 10:41:14 -0300

    commit certo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Usuario } from './models/usuario';
 
+
 function App() {
-  const usuario = new Usuario('Lucas', 28, '123456');
-
-
+  const usuario = new Usuario('joão', 28);
+  const usuario2 = new Usuario('maria', 25);
   return (
     <div style={{ padding: '2rem' }}>
       <h1>{usuario.apresentar()}</h1>
+      <h1>{usuario2.apresentar()}</h1>
       <p>
       </p>
     </div>
--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -1,15 +1,12 @@
 export class Usuario {
   constructor(
-    public nome: string,
-    public idade: number,
-    private senha: string
+    private nome: string,
+    private idade: number,
   ) {}
 
   apresentar(): string {
     return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`;
   }
 
-  verificarSenha(tentativa: string): boolean {
-    return this.senha === tentativa;
+ ;
   }
-}
